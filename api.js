6783b75c93989d22b16b2bbef1d3bf26464c8165
JackyE97 <zellertln@gmail.com>
2024-06-11 23:15:57 +0300

    убрала применения тегов

--- api.js
+++ api.js
@@ -57,3 +57,4 @@ export function postApi(name, text) {
       return "error";
     })
 }
+
--- render.js
+++ render.js
@@ -8,11 +8,19 @@ export const renderCommentators = () => {
     .map((commentator, index) => {
       return `<li class="comment" data-index="${index}" data-name="${commentator.name}">
         <div class="comment-header">
-          <div>${commentator.name}</div>
+          <div>${commentator.name.replaceAll("&", "&amp;")
+          .replaceAll("<", "&lt;")
+          .replaceAll(">", "&gt;")
+          .replaceAll('"', "&quot;")}</div>
           <div>${commentator.time}</div>
         </div>
         <div class="comment-body">
-          <div class="comment-text">${commentator.comment}</div>
+          <div class="comment-text">${commentator.comment.replaceAll("&", "&amp;")
+          .replaceAll("<", "&lt;")
+          .replaceAll(">", "&gt;")
+          .replaceAll('"', "&quot;")
+          .replaceAll('QUOTE_BEGIN', "<div class='quote'>")
+          .replaceAll('QUOTE_END', "</div>")}
         </div>
         <div class="comment-footer">
           <div class="likes">
